0459265e718c58d41f6e7f74b8f7aeefb3e875d3
Jose Alvarado Torre <jos.alvatorre@gmail.com>
2018-09-18 23:17:54 -0700

    overlay complete

--- js/login.js
+++ js/login.js
@@ -9,9 +9,36 @@ document.addEventListener("DOMContentLoaded", event => {
 //firebase.initializeApp(config);
 //look in the HTML for config
 
+function toggleOverlay(){
+
+  let self = userTypeOnSubmit;
+
+  let el = document.querySelector('#overlay');
+  let el_display = el.style.display;
+
+  if ( el_display == '' && !self.calledMoreThanOnce )
+  {
+    self.calledMoreThanOnce = true;
+    el.style.display = 'flex';
+  }
+  else if ( el_display == 'none' )
+  {
+    el.style.display = 'flex';
+  }
+  else if ( el_display == 'flex' )
+  {
+    el.style.display = 'none';
+  }
+  else
+  {
+    throw `${self.name}: ${el.id} had a display value of '${el_display}'.
+    It should only toggle between 'flex' and 'none'`;
+  }
+}
+
 function userTypeOnSubmit() 
 {
-   
+  toggleOverlay();
 }
 
 function googleLogin()
