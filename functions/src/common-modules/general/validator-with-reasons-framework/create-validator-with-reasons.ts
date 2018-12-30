af4886c906ee1c7883cb52e7a5178a14c1b9079a
Jose Alvarado Torre <jos.alvatorre@gmail.com>
2018-12-30 01:01:01 -0800

    a small correction to createValidatorWithReasons before going to sleep

--- functions/src/common-modules/general/validator-with-reasons-framework/create-validator-with-reasons.ts
+++ functions/src/common-modules/general/validator-with-reasons-framework/create-validator-with-reasons.ts
@@ -9,30 +9,36 @@ import { Reason } from './Reason';
  * argument pass of a series of tests.
  * If any of these tests fail, the validator fails.
  * 
- * We want a special validator function that will –instead of returning false–
- * tell us which test failed and give a Reason as to why it failed.
- * 
- * Type T determines the type of the argument
- * that the validatos should take.
+ * We want a special validator function that will 
+ * –instead of simply returning false–
+ * tell us which test failed and give Reasons as to why it failed.
  * 
  * The tests and corresponding Reasons are
  * provided through the map parameter.
+ * 
+ * Type T determines the type of the argument
+ * that the validator should take.
  * 
  * @param test_TO_reasonIfFailed a map from
  * each test function (i.e. a function with on parameter of type T)
  * to the Reason explaining why it would return false.
  * The test should take as parameters
- * the object to be tested and a boolean to specify whether
- * the validator should return false or an array of reasons
- * if any of the tests fail.
+ * the object to be tested.
+ * 
+ * @returns the validator function. It takes as
+ * parameters an object of type T and
+ * a boolean specifying whether the validator should
+ * return false or an array of Reasons
+ * if the object is considered invalid.
  */
 export function createValidatorWithReasons<T>(
   test_TO_reasonForFailure: Map<
-    ( x: T, returnReasonsIfFailure?: boolean ) => boolean,
+    ( x: T ) => boolean,
     Reason
   >
 )
-: ( x: T ) => boolean | Array<Reason>
+: // returns the validator function
+  ( x: T, returnReasonsIfFailure?: boolean ) => boolean | Array<Reason>
 {
   //TODO
 }
