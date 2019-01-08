acaa7f97844d53d1aa2827505f6624bf995f0a11
Jose Alvarado Torre <jos.alvatorre@gmail.com>
2019-01-07 23:52:54 -0800

    end-of-day 07 Jan 2019

--- angular-website/src/app/classes/breakpoint-subscriber.class.ts
+++ angular-website/src/app/classes/breakpoint-subscriber.class.ts
@@ -20,168 +20,54 @@ export class BreakpointSubscriber {
 
   private activeSubscriptions: Subscription[];
 
-  /**
-   * map from a breakpoint property name to the respective callbacks.
-   */
-  private breakpoint_TO_callbacks: Map<
-    string,
-    { greaterEqual: () => void, lessThan: () => void }
-  > = new Map<
-    string,
-    { greaterEqual: () => void, lessThan: () => void }
-  >();
+  public readonly deviceBreakpointsDescending = Object.freeze([
+    Breakpoints.Web, Breakpoints.Tablet, Breakpoints.Handset
+  ]);
 
-  constructor( private breakpointObserver: BreakpointObserver ) {
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
-    // this.activeSubscriptions =
-    // breakpointObserver.observe([
-    //   Breakpoints.Handset,
-    //   Breakpoints.Tablet,
-    //   Breakpoints.Web
-    // ])
-    // .subscribe(( value: BreakpointState ) => {
-    // });
+  getCurrentDeviceBreakpoint(): string {
+
+    for ( const break_pt of this.deviceBreakpointsDescending ) {
+      if (this.breakpointObserver.isMatched( break_pt )) {
+        return break_pt;
+      }
+    }
+
+    // if this goes below the lowest breakpoint,
+    // then just return the lowest breakpoint.
+    return this.deviceBreakpointsDescending[
+      this.deviceBreakpointsDescending.length - 1
+    ];
   }
 
-  setBreakpointCallbacks(
-    breakpoints_TO_callbacks: Map<
-      string,
-      {
-        greaterEqualCallback: null | (() => void),
-        lessThanCallback: null | (() => void),
-        callback?: undefined
-      }
-      | {
-        greaterEqualCallback?: undefined,
-        lessThanCallback?: undefined,
-        callback: null | (( _: BreakpointState  ) => void)
-      }
-    >
+  setDeviceTransitionCallbacks(
+    toHandset: null | (() => void),
+    toTablet: null | (() => void),
+    toWeb: null | (() => void)
   ): void {
 
-    // SECTION: breakpoint validation
+    const self = this.setDeviceTransitionCallbacks;
 
-    for ( const [breakpoint, callbacks] of breakpoints_TO_callbacks ) {
+    // SECTION: set static variables
 
-      this.activeSubscriptions.push(
-        this.breakpointObserver.observe( breakpoint ).subscribe(
-          ( breakpointState: BreakpointState ) => {
+    const _calledBefore = 'calledBefore';
 
-            if ( callbacks.callback ) {
-              callbacks.callback( breakpointState );
-            }
-
-            if ( breakpointState.matches && callbacks.greaterEqualCallback ) {
-              callbacks.greaterEqualCallback();
-            }
-
-            if ( !breakpointState.matches && callbacks.lessThanCallback ) {
-              callbacks.lessThanCallback();
-            }
-          }
-        )
-      );
-    }
-  }
-
-  ifBreakpoint(
-    breakpoint: string,
-    greaterEqualCallback: null | (() => void),
-    lessThanCallback: null | (() => void)
-  ): void {
-
-    const self = this.ifBreakpoint;
-
-    // SECTION: validate parameters
-
-    // if the given breakpoint property name doesn't exist,
-    if ( !Breakpoints[breakpoint] ) {
-      throw Error(
-        'breakpoint not incuded in @angular/cdk/layouts Breakpoints'
-      );
-    }
-
-    // SECTION: define static variables
-
-    // A map from a breakpoint property name to the respective callbacks.
-    const _breakpoint_TO_callbacks = 'breakpoint_TO_callbacks';
-
-    // if the static variable hasn't been defined yet,
-    if (!self[_breakpoint_TO_callbacks]) {
-
-      // then define it for the first time.
-      Object.defineProperty(
-        // assign it to this function
-        self,
-        // property name
-        _breakpoint_TO_callbacks,
-        {
-          configurable: false,
-          enumerable: false,
-          writable: false,
-          value: new Map<
-            string,
-            { greaterEqual: null | (() => void), lessThan: null | (() => void) }
-          >()
-        }
-      );
-    }
-
-    const breakpoint_TO_callbacks:  Map<
-      string,
-      { greaterEqual: null | (() => void), lessThan: null | (() => void) }
-    > = self[ _breakpoint_TO_callbacks ];
-
-    // SECTION:
-
-
-    // if this breakpoint hasn't been added to the map,
-    if (!this.breakpoint_TO_callbacks.has( breakpoint )) {
+    if (!self[_calledBefore]) {
 
       this.activeSubscriptions.push(
-        this.breakpointObserver.observe( breakpoint ).subscribe(
-          ( breakpointState: BreakpointState ) => {
-
-            const callbacks = this.breakpoint_TO_callbacks.get( breakpoint );
-
-            if (!callbacks) {
-              console.error(
-                `logic error: ${breakpoint} entry got deleted from map.`
-              );
-              return;
-            }
-
-            const { greaterEqual, lessThan } = callbacks;
-
-            if (this.breakpoint_TO_callbacks.has( breakpoint )) {
-              const {
-                greaterEqual,
-                lessThan
-              } = this.breakpoint_TO_callbacks.get( breakpoint );
-            }
-
-            if ( breakpointState.matches ) {
-              if ( greaterEqualCallback ) {
-                greaterEqualCallback();
-              }
-            } else {
-              if ( lessThanCallback ) {
-                lessThanCallback();
-              }
-            }
-          }
-        )
+        this.breakpointObserver.observe(
+          // copy the ReadonlyArray into a string[]
+          this.deviceBreakpointsDescending.slice()
+        ).subscribe(( next: BreakpointState ) => {
+          // TODO
+        })
       );
+
+      self[_calledBefore] = true;
     }
-  }
-
-  reactToBreakpointGreaterEqual( breakpoint: string, callback: (() => void) ) {
-
-    if ( !Breakpoints[breakpoint] ) {
-      throw Error('Breakpoint non-existent');
-    }
-
-
   }
 
   /**
