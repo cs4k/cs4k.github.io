c1b5ff2e1b0a935b09740d7d53a33242656d8999
Jose Alvarado Torre <jos.alvatorre@gmail.com>
2019-01-07 19:45:33 -0800

    about to simplify breakpoint-subscriber.class.ts.

--- angular-website/src/app/classes/breakpoint-subscriber.class.ts
+++ angular-website/src/app/classes/breakpoint-subscriber.class.ts
@@ -43,19 +43,99 @@ export class BreakpointSubscriber {
     // });
   }
 
+  setBreakpointCallbacks(
+    breakpoints_TO_callbacks: Map<
+      string,
+      {
+        greaterEqualCallback: null | (() => void),
+        lessThanCallback: null | (() => void),
+        callback?: undefined
+      }
+      | {
+        greaterEqualCallback?: undefined,
+        lessThanCallback?: undefined,
+        callback: null | (( _: BreakpointState  ) => void)
+      }
+    >
+  ): void {
+
+    // SECTION: breakpoint validation
+
+    for ( const [breakpoint, callbacks] of breakpoints_TO_callbacks ) {
+
+      this.activeSubscriptions.push(
+        this.breakpointObserver.observe( breakpoint ).subscribe(
+          ( breakpointState: BreakpointState ) => {
+
+            if ( callbacks.callback ) {
+              callbacks.callback( breakpointState );
+            }
+
+            if ( breakpointState.matches && callbacks.greaterEqualCallback ) {
+              callbacks.greaterEqualCallback();
+            }
+
+            if ( !breakpointState.matches && callbacks.lessThanCallback ) {
+              callbacks.lessThanCallback();
+            }
+          }
+        )
+      );
+    }
+  }
+
   ifBreakpoint(
     breakpoint: string,
     greaterEqualCallback: null | (() => void),
     lessThanCallback: null | (() => void)
   ): void {
 
+    const self = this.ifBreakpoint;
+
+    // SECTION: validate parameters
+
+    // if the given breakpoint property name doesn't exist,
     if ( !Breakpoints[breakpoint] ) {
       throw Error(
         'breakpoint not incuded in @angular/cdk/layouts Breakpoints'
       );
     }
 
-    // if this breakpoint hasn't been added to the map
+    // SECTION: define static variables
+
+    // A map from a breakpoint property name to the respective callbacks.
+    const _breakpoint_TO_callbacks = 'breakpoint_TO_callbacks';
+
+    // if the static variable hasn't been defined yet,
+    if (!self[_breakpoint_TO_callbacks]) {
+
+      // then define it for the first time.
+      Object.defineProperty(
+        // assign it to this function
+        self,
+        // property name
+        _breakpoint_TO_callbacks,
+        {
+          configurable: false,
+          enumerable: false,
+          writable: false,
+          value: new Map<
+            string,
+            { greaterEqual: null | (() => void), lessThan: null | (() => void) }
+          >()
+        }
+      );
+    }
+
+    const breakpoint_TO_callbacks:  Map<
+      string,
+      { greaterEqual: null | (() => void), lessThan: null | (() => void) }
+    > = self[ _breakpoint_TO_callbacks ];
+
+    // SECTION:
+
+
+    // if this breakpoint hasn't been added to the map,
     if (!this.breakpoint_TO_callbacks.has( breakpoint )) {
 
       this.activeSubscriptions.push(
