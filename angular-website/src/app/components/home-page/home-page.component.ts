152caa3f0ff5286a8f0e0c2fa6875ca28797d229
Jose Alvarado Torre <jos.alvatorre@gmail.com>
2019-02-04 18:58:57 -0800

    moved to sass. problems installing

--- angular-website/src/app/components/home-page/home-page.component.ts
+++ angular-website/src/app/components/home-page/home-page.component.ts
@@ -11,7 +11,7 @@ import {
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit, OnDestroy {
 
