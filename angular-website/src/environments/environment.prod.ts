a3a1955be587a054d399b2d694140d27b5b218fb
Jose <jos.alvatorre@gmail.com>
2019-02-27 07:35:41 -0800

    Just deployed to Firebase.

--- angular-website/src/environments/environment.prod.ts
+++ angular-website/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBUdKM4sezYfxczl-aOSqBzg45C5kIOUOI",
+    authDomain: "cs4kwebsite.firebaseapp.com",
+    databaseURL: "https://cs4kwebsite.firebaseio.com",
+    projectId: "cs4kwebsite",
+    storageBucket: "cs4kwebsite.appspot.com",
+    messagingSenderId: "804641345230"
+  }
 };
