4ea7e47686fd3e4ec62b7d5e728d349014deeb7a
Jose <jos.alvatorre@gmail.com>
2019-03-27 12:28:44 -0700

    Before new branches
    volunteer-page and server-side-rendering branches will be created.
    Before that happens, home-page will be the default page and volunteer-page will be inaccessible to the user.

--- angular-website/src/app/app-routing.module.ts
+++ angular-website/src/app/app-routing.module.ts
@@ -4,11 +4,9 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { VolunteerPageComponent } from './components/volunteer-page/volunteer-page.component';
 
 const routes: Routes = [
-  // '' should redirect to 'home'. However, it'll redirect to
-  // volunteers for now to make it easier for testing the volunteer page
-  { path: '', redirectTo: 'volunteers', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
-  { path: 'volunteers', component: VolunteerPageComponent }
+  // { path: 'volunteers', component: VolunteerPageComponent }
   // TODO give 404 error to the catch-all path '**'
 ];
 
