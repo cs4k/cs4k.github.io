a8714ff0ebe087e53e89e060d50dd75b406aff4d
Jingyi Zhu <jingyizhu@Jingyis-MacBook-Pro.local>
2020-04-15 20:42:28 -0700

    add sponsors

--- angular-website/src/app/app.module.ts
+++ angular-website/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 // required for AngularFire2
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
 // optional AngularFire2 modules for specific needs
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
 // import { AngularFireFunctionsModule } from '@angular/fire/functions';
 // import { AngularFirestoreModule } from '@angular/fire/firestore';
 // import { AngularFireStorageModule } from '@angular/fire/storage';
@@ -24,21 +24,22 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 // ng-bootstrap module and widgets
 // import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AuthService } from './services/auth.service';
-import { NavComponent } from './components/nav/nav.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { TeamComponent } from './components/team/team.component';
-import { FaqComponent } from './components/faq/faq.component';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { VolunteerPageComponent } from './components/volunteer-page/volunteer-page.component';
-import { FeaturedVolunteersComponent } from './components/featured-volunteers/featured-volunteers.component';
-import { CurrentVolunteersComponent } from './components/current-volunteers/current-volunteers.component';
-import { PastVolunteersComponent } from './components/past-volunteers/past-volunteers.component';
-import { PhotosComponent } from './components/photos/photos.component';
-import { NewsComponent } from './components/news/news.component';
-import { SchoolContactComponent } from './components/school-contact/school-contact.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
+import { AuthService } from "./services/auth.service";
+import { NavComponent } from "./components/nav/nav.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { TeamComponent } from "./components/team/team.component";
+import { FaqComponent } from "./components/faq/faq.component";
+import { ContactUsComponent } from "./components/contact-us/contact-us.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { VolunteerPageComponent } from "./components/volunteer-page/volunteer-page.component";
+import { FeaturedVolunteersComponent } from "./components/featured-volunteers/featured-volunteers.component";
+import { CurrentVolunteersComponent } from "./components/current-volunteers/current-volunteers.component";
+import { PastVolunteersComponent } from "./components/past-volunteers/past-volunteers.component";
+import { PhotosComponent } from "./components/photos/photos.component";
+import { NewsComponent } from "./components/news/news.component";
+import { SchoolContactComponent } from "./components/school-contact/school-contact.component";
+import { CalendarComponent } from "./components/calendar/calendar.component";
+import { SponsorsComponent } from "./components/sponsors/sponsors.component";
 
 @NgModule({
   declarations: [
@@ -56,10 +57,11 @@ import { CalendarComponent } from './components/calendar/calendar.component';
     PastVolunteersComponent,
     NewsComponent,
     SchoolContactComponent,
-    CalendarComponent
+    CalendarComponent,
+    SponsorsComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
@@ -68,7 +70,7 @@ import { CalendarComponent } from './components/calendar/calendar.component';
     AngularFireModule.initializeApp(
       environment.firebase,
       // app name
-      'angular-website'
+      "angular-website"
     ),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
@@ -85,9 +87,7 @@ import { CalendarComponent } from './components/calendar/calendar.component';
     // ng-bootstrap modules
     // NgbModule
   ],
-  providers: [
-    AuthService
-  ],
-  bootstrap: [AppComponent]
+  providers: [AuthService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
